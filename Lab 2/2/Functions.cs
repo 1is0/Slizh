b5b21be8855912cfc1744c9688d963c2892f1eec
simmaksim <62940225+simmaksim@users.noreply.github.com>
2020-03-31 21:00:04 +0300

    Вы это уже проверяли но оценку не выставили из-за CamelCase

---
+++ Lab 2/2/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+    class Functions
+    {
+    public static void Calculating(string str)
+    {
+        int[] counters = new int[10];
+        //char tmp = '0';
+        foreach (char j in str)
+        {
+            if (j >= '0' && j <= '9')
+            {
+                counters[j - '0']++;
+            }
+
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            if (counters[i] != 0)
+            {
+                Console.WriteLine("There are\t" + counters[i] + "\t" + i);
+            }
+        }
+    }
+}
+
---
+++ Lab 2/2/TimeFormats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+    class TimeFormats
+    {
+        static void Main()
+        {
+            System.DateTime time = DateTime.Now;
+            string firstFormat = time.ToString("R");
+            string secondFormat = time.ToString("G");
+            Console.WriteLine(firstFormat);
+            Console.WriteLine(secondFormat);
+            Functions.Calculating(firstFormat);
+            Console.WriteLine("------------------------------------");
+            Functions.Calculating(secondFormat);
+        }
+    }
+    
+
