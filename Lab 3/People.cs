f22c94c0b5505df39980f4df56849699304461a1
simmaksim <62940225+simmaksim@users.noreply.github.com>
2020-03-31 21:09:10 +0300

    Add files via upload

---
+++ Lab 3/People.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class People
+    {
+        Person[] data;
+        public People(int quantity)
+        {
+            data = new Person[quantity];
+        }
+        public Person this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+    }
+}
