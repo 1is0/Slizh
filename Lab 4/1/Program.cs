20e37dc837ae9e5d55b68280d8ea40f70ad8d641
simmaksim <62940225+simmaksim@users.noreply.github.com>
2020-04-03 18:01:02 +0300

    Add files via upload

---
+++ Lab 4/1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace LB4
+{
+    class Program
+    {
+
+
+        [DllImport("User32.dll", EntryPoint = "GetAsyncKeyState")]
+        public static extern int Key(int x);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Thread.Sleep(100);
+                //Немного побаловался с выводом
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = Key(i);
+                    if (keyState == 32768)
+                    {
+                        Console.WriteLine("You pressed - {0}", (char)((char)i));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lab 4/2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LB4_2_
+{
+
+    class Program
+    {
+        [DllImport("C:\\Projects\\c#\\lab4\\2nd\\Dll\\Debug\\Dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Sum(int a, int b);
+
+        [DllImport("C:\\Projects\\c#\\lab4\\2nd\\Dll\\Debug\\Dll.dll")]
+        public static extern int Diff(int a, int b);
+
+        [DllImport("C:\\Projects\\c#\\lab4\\2nd\\Dll\\Debug\\Dll.dll")]
+
+        public static extern int Division(int a, int b);
+
+        [DllImport("C:\\Projects\\c#\\lab4\\2nd\\Dll\\Debug\\Dll.dll")]
+
+        public static extern int Fact(int a);
+
+        [DllImport("C:\\Projects\\c#\\lab4\\2nd\\Dll\\Debug\\Dll.dll")]
+
+        public static extern int Module(int a);
+
+        static void Main(string[] args)
+        {
+            int sum = Sum(10, 20);
+            int dif = Diff(30, 20);
+            int div = Division(14, 2);
+            int fact = Fact(7);
+            int mod = Module(-6);
+
+            Console.WriteLine($"10 + 20 = {sum.ToString()}");
+            Console.WriteLine($"30 - 20 = {dif.ToString()}");
+            Console.WriteLine($"14 : 2 = {div.ToString()}");
+            Console.WriteLine($"Fact 7 = {fact.ToString()}");
+            Console.WriteLine($"Module -6 = {mod.ToString()}");
+        }
+    }
+}
