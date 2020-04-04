c9dcc1a8d707df80548e2c8a7b632cdbe29f3b41
simmaksim <62940225+simmaksim@users.noreply.github.com>
2020-04-04 06:13:37 +0300

    Add files via upload

---
+++ Lab 5/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Footballers
+{
+    abstract class Person
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        static int ID = 1000000;
+        
+        public Person()
+        {
+            Name = "Ronaldinho";
+            Surname = "de Assis Moreira";
+            Country = "Brazil";
+            ID += 1;
+        }
+        public Person (string s, string n, string c)
+        {
+            Surname = s;
+            Name = n;
+            Country = c;
+            ID += 1;
+        }
+        public override string ToString() => $"Name: {Name}\t Surname: {Surname}\nCountry: {Country}\nID: {ID}";
+    }
+}
