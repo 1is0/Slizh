3d94dd277ce1cb41aa2d79c04630cb62e84b12e5
simmaksim <62940225+simmaksim@users.noreply.github.com>
2020-05-05 17:09:46 +0300

    Add files via upload

---
+++ C#/Lab 7/Number.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Number : IEquatable<Number>, IComparable
+    {
+        double value;
+
+        public int Numerator { get; set; }
+
+        public int Denominator { get; set; }
+
+        public Number()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        public Number(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+            value = (double)numerator / denominator;
+        }
+
+        public Number(double x)
+        {
+            value = x;
+
+            if (x % 10 == 0)
+            {
+                Numerator = (int)x;
+                Denominator = 1;
+            }
+
+            else
+            {
+                int i = 0;
+                while (x % 10 != 0)
+                {
+                    x *= 10;
+                    i++;
+                }
+
+                i--;
+                x /= 10;
+
+                Numerator = (int)x;
+                Denominator = (int)Math.Pow(10, i);
+
+                Decrease();
+            }
+        }
+
+        public static Number operator +(Number firstNumber, Number secondNumber)
+        {
+            Number num = new Number();
+
+            num.Numerator = firstNumber.Numerator * secondNumber.Denominator + firstNumber.Denominator * secondNumber.Numerator;
+            num.Denominator = firstNumber.Denominator * secondNumber.Denominator;
+            num.value = firstNumber.value + secondNumber.value;
+
+            num.Decrease();
+
+            return num;
+        }
+
+        public static Number operator -(Number n1, Number n2)
+        {
+            Number num = new Number();
+
+            num.Numerator = n1.Numerator * n2.Denominator - n1.Denominator * n2.Numerator;
+            num.Denominator = n1.Denominator * n2.Denominator;
+            num.value = n1.value - n2.value;
+
+            num.Decrease();
+
+            return num;
+        }
+        public static Number operator *(Number n1, Number n2)
+        {
+            Number num = new Number();
+
+            num.Numerator = n1.Numerator * n2.Numerator;
+            num.Denominator = n1.Denominator * n2.Denominator;
+            num.value = n1.value * n2.value;
+
+            num.Decrease();
+
+            return num;
+        }
+        public static Number operator /(Number n1, Number n2)
+        {
+            Number num = new Number();
+
+            num.Numerator = n1.Numerator * n2.Denominator;
+            num.Denominator = n1.Denominator * n2.Numerator;
+            num.value = n1.value / n2.value;
+            if (n1 < 0 || n2 < 0)
+            {
+                num.Numerator = -num.Numerator;
+                num.Denominator = Math.Abs(num.Denominator);
+            }
+
+            num.Decrease();
+
+            return num;
+        }
+        public bool Equals(Number other) => value == other.value;
+
+        public int CompareTo(object obj)
+        {
+            if (this.value > ((Number)obj).value)
+            {
+                return 1;
+            }
+
+            if (this.value < ((Number)obj).value)
+            {
+                return -1;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool operator <(Number firstNumber, Number secondNumber) => firstNumber.value < secondNumber.value;
+
+        public static bool operator >(Number firstNumber, Number secondNumber) => firstNumber.value > secondNumber.value;
+
+        public static bool operator <=(Number firstNumber, Number secondNumber) => firstNumber.value <= secondNumber.value;
+
+        public static bool operator >=(Number firstNumber, Number secondNumber) => firstNumber.value >= secondNumber.value;
+
+        public static bool operator ==(Number firstNumber, Number secondNumber) => firstNumber.Equals(secondNumber);
+
+        public static bool operator !=(Number firstNumber, Number secondNumber) => !firstNumber.Equals(secondNumber);
+
+        public static explicit operator int(Number number) => (int)number;
+
+        public static implicit operator double(Number number) => number.value;
+
+
+        public override string ToString()
+        {
+            if ((value - (int)value) != 0)
+            {
+                return $"{Numerator}/{Denominator}";
+            }
+            else
+            {
+                return $"{value}";
+            }
+        }
+
+        void Decrease()
+        {
+            int GCT(int x, int y)
+            {
+                x = Math.Abs(x);
+                y = Math.Abs(y);
+                while (x != 0 && y != 0)
+                {
+                    if (x >= y)
+                        x %= y;
+
+                    else
+                        y %= x;
+                }
+
+                return x | y;
+            }
+
+            while (GCT(Numerator, Denominator) != 1)
+            {
+                int n = GCT(Numerator, Denominator);
+                Numerator /= n;
+                Denominator /= n;
+            }
+        }
+    }
+}
---
+++ C#/Lab 7/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        public static double GetDouble()
+        {
+
+            double tmp;
+
+            while (!double.TryParse(Console.ReadLine(), out tmp))
+            {
+                Console.Write("Incorrect input, repeat: ");
+            }
+
+            return tmp;
+        }
+
+        public static Number GettingFractionalNumber()
+        {
+            Console.Write("\nEnter fractional number(in x/y format): ");
+
+            string tempString = Console.ReadLine();
+            while (!CheckFractionalNumber(tempString))
+            {
+
+                Console.Write("Incorrect input, repeat: ");
+                tempString = Console.ReadLine();
+            }
+
+            try
+            {
+                string[] mas = tempString.Split('/');
+                return new Number(int.Parse(mas[0]), int.Parse(mas[1]));
+            }
+
+            catch
+            {
+                return new Number(int.Parse(tempString), 1);
+            }
+        }
+
+        public static Number GetNumber()
+        {
+            Console.Write("\nEnter natural number: ");
+            double number = GetDouble();
+            return new Number(number);
+        }
+
+        public static Number GettingRationalNumber()
+        {
+            int x;
+            Console.Write("Press 1 to enter fractional number, 2 to enter natural number ");
+
+
+            while (!int.TryParse(Console.ReadLine(), out x) || x < 1 || x > 2)
+            {
+                Console.Write("Incorrect input, repeat: ");
+            }
+
+            if (x == 1)
+            {
+                return GettingFractionalNumber();
+            }
+
+            else
+            {
+                return GetNumber();
+            }
+        }
+
+        public static bool CheckFractionalNumber(string frac)
+        {
+            if (String.IsNullOrEmpty(frac))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < frac.Length; i++)
+            {
+                if (i == 0 && frac[i] == '-')
+                {
+                    i++;
+                }
+
+                if ((frac[i] == '/' && i != frac.Length - 1))
+                {
+                    i++;
+                }
+
+                if (frac[i] == '0' && frac.Length == 1)
+                {
+                    return false;
+                }
+
+                if (!char.IsDigit(frac[i]) || (frac[i] == '0' && i == frac.Length - 1 && frac[i - 1] == '/'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter the first number");
+            var firstNumber = GettingRationalNumber();
+            Console.Clear();
+            Console.Write("Enter the second number");
+            var secondNumber = GettingRationalNumber();
+
+            Console.Clear();
+            Console.WriteLine("The first number: " + firstNumber);
+            Console.WriteLine("The second number: " + secondNumber);
+
+            Console.Write("{0} + {1} = ", firstNumber, secondNumber);
+            Console.WriteLine(firstNumber + secondNumber);
+
+            Console.Write("{0} - {1} = ", firstNumber, secondNumber);
+            Console.WriteLine(firstNumber - secondNumber);
+
+            Console.Write("{0} * {1} = ", firstNumber, secondNumber);
+            Console.WriteLine(firstNumber * secondNumber);
+
+            Console.Write("{0} / {1} = ", firstNumber, secondNumber);
+            Console.WriteLine(firstNumber / secondNumber);
+
+            Console.Write("{0} > {1} = ", firstNumber, secondNumber);
+            Console.WriteLine(firstNumber > secondNumber);
+
+            Console.Write("{0} < {1} = ", firstNumber, secondNumber);
+            Console.WriteLine(firstNumber < secondNumber);
+
+            Console.Write("{0} == {1} = ", firstNumber, secondNumber);
+            Console.WriteLine(firstNumber == secondNumber);
+
+            Console.Write("{0} != {1} = ", firstNumber, secondNumber);
+            Console.WriteLine(firstNumber != secondNumber);
+        }
+
+    }
+}
